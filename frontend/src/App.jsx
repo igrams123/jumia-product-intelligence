96ee4752cf220b79291973171173ff1aa3fcc790
Billy obunde <84906948+igrams123@users.noreply.github.com>
2026-05-27 13:40:42 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ import {
   FaBoxOpen,
 } from "react-icons/fa";
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const brands = [
   "Samsung",
