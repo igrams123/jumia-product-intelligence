a6b724e1b755cc5d9408d7f254ae0058896d08f8
igrams123 <obundebilly@gmail.com>
2026-05-27 16:40:07 +0300

    fix neon database pooling

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,6 +3,9 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
@@ -10,7 +13,9 @@ print("CONNECTED TO:", DATABASE_URL)
 
 engine = create_engine(
     DATABASE_URL,
-    echo=True
+    echo=True,
+    pool_pre_ping=True,
+    pool_recycle=300
 )
 
 SessionLocal = sessionmaker(
@@ -21,7 +26,9 @@ SessionLocal = sessionmaker(
 
 Base = declarative_base()
 
+
 def get_db():
+
     db = SessionLocal()
 
     try:
